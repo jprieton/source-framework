44dd03a475780860176e00c320bfddb5f01f15a8
Javier Prieto <jprieton@gmail.com>
2017-03-17 10:25:39 -0430

    Avoid direct access to file

--- includes/option.php
+++ includes/option.php
@@ -1,6 +1,13 @@
 <?php
 
 /**
+ * If this file is called directly, abort.
+ */
+if ( !defined( 'ABSPATH' ) ) {
+  die( 'Direct access is forbidden.' );
+}
+
+/**
  * Retrieves an option boolean value based on an option name.
  *
  * @since 1.0.0
