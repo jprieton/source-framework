1f03d83156bece371ffffab284132d9af0d4e512
Javier Prieto <jprieton@gmail.com>
2017-03-24 14:52:12 -0430

    Bootstrap

---
+++ bootstrap/bootstrap-init.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace SourceFramework\Bootstrap;
+
+/**
+ * If this file is called directly, abort.
+ */
+if ( !defined( 'ABSPATH' ) ) {
+  die( 'Direct access is forbidden.' );
+}
+
+/**
+ * Bootstrap_Init class
+ *
+ * @package        Bootstrap
+ * @subpackage     Init
+ * @since          1.0.0
+ * @author         Javier Prieto <jprieton@gmail.com>
+ */
+final class Bootstrap_Init {
+
+  /**
+   * Static instance of this class
+   *
+   * @since         1.0.0
+   * @var           AdminInit
+   */
+  protected static $instance;
+
+  /**
+   * @since         1.0.0
+   * @return  static
+   */
+  public static function &get_instance() {
+    if ( !isset( static::$instance ) ) {
+      static::$instance = new static;
+    }
+    return static::$instance;
+  }
+
+  /**
+   * Declared as protected to prevent creating a new instance outside of the class via the new operator.
+   *
+   * @since         1.0.0
+   */
+  protected function __construct() {
+
+  }
+
+  /**
+   * Declared as private to prevent cloning of an instance of the class via the clone operator.
+   *
+   * @since         1.0.0
+   */
+  private function __clone() {
+
+  }
+
+  /**
+   * declared as private to prevent unserializing of an instance of the class via the global function unserialize().
+   *
+   * @since         1.0.0
+   */
+  private function __wakeup() {
+
+  }
+
+  /**
+   * Declared as protected to prevent serializg of an instance of the class via the global function serialize().
+   *
+   * @since         1.0.0
+   */
+  protected function __sleep() {
+
+  }
+
+}
