b8418d1292aa58f0906563867853734b5aec4ac9
Javier Prieto <jprieton@gmail.com>
2017-04-10 15:59:45 -0430

    Build upgrade

---
+++ build.php
@@ -0,0 +1,22 @@
+<?php
+
+if ( file_exists( 'source-framework.phar' ) ) {
+  unlink( 'source-framework.phar' );
+}
+
+$phar = new Phar( 'source-framework.phar' );
+$phar->setStub( '<?php __HALT_COMPILER();' );
+
+$folders = [ 'admin', 'public' ];
+
+foreach ( $folders as $folder ) {
+  $files = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $folder ), RecursiveIteratorIterator::SELF_FIRST );
+
+  foreach ( $files as $file ) {
+    if ( preg_match( '/\.php$/', $file ) ) {
+      $file instanceof SplFileInfo;
+      $phar->addFromString( $file->getPathname(), php_strip_whitespace( $file->getPathname() ) );
+    }
+  }
+}
+$phar->compressFiles( Phar::GZ );
