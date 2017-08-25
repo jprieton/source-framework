8ffc6855dd832b25bdf5c26eb08508a2290a8b28
Javier Prieto <jprieton@gmail.com>
2017-08-25 16:21:20 -0430

    Breadcrumb

---
+++ Template/Bootstrap4/Breadcrumb.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace SourceFramework\Template\Bootstrap4;
+
+use SourceFramework\Template\Tag;
+
+/**
+ * Description of Breadcrumb
+ *
+ * @author perseo
+ */
+class Breadcrumb extends \SourceFramework\Template\Breadcrumb {
+
+  public function generate() {
+    $breadcrumb = parent::generate();
+
+    $count = count( $breadcrumb );
+    $list = [];
+    $i    = 0;
+    foreach ( $breadcrumb as $crumb ) {
+      $i++;
+      $li     = ($i < $count) ? 'li.breadcrumb-item' : 'li.breadcrumb-item.active';
+      $link   = ($i < $count) ? Tag::a( $crumb[1], $crumb[0] ) : $crumb[0];
+      $list[] = Tag::html( $li, $link );
+    }
+
+    return Tag::html( 'ol.breadcrumb', implode( "\n", $list ) );
+  }
+
+}
