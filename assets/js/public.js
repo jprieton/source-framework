2c15e8c92f2e2be6056e527122efdfdb5e452226
Javier Prieto <jprieton@gmail.com>
2018-05-28 15:55:54 -0430

    onChange event for select.goto-url

--- assets/js/public.js
+++ assets/js/public.js
@@ -1,4 +1,10 @@
 (function ($) {
   'use strict';
 
+  // Go to url on select change
+  $('body').on('change', 'select.goto-url', function () {
+    let url = $(this).val();
+    window.location.assign(url);
+  });
+
 })(jQuery);
