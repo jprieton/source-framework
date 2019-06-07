ba7431b4dabbf6d66ea7b4aa07729fb70c5fc726
jprieton <jprieton@gmail.com>
2019-06-07 11:52:03 -0400

    Update requirements

--- source-framework.php
+++ source-framework.php
@@ -4,7 +4,7 @@
  * Plugin Name:   SourceFramework
  * Plugin URI:    https://github.com/jprieton/source-framework
  * Description:   An extensible object-oriented micro-framework for WordPress that helps you to develop themes and plugins.
- * Version:       2.0.0
+ * Version:       2.2.0
  * Author:        Javier Prieto
  * Author URI:    https://github.com/jprieton
  * Text Domain:   source-framework
@@ -27,32 +27,19 @@
  */
 
 // If this file is called directly, abort.
-if ( !defined( 'ABSPATH' ) ) {
-  die( 'Direct access is forbidden.' );
-}
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Define plugin constants
  * @since 1.0.0
  */
-define( 'SF_VERSION', '2.0.0' );
+define( 'SF_VERSION', '2.2.0' );
 define( 'SF_FILENAME', __FILE__ );
 define( 'SF_BASENAME', plugin_basename( __FILE__ ) );
 define( 'SF_BASEDIR', __DIR__ );
 define( 'SF_BASEURL', plugin_dir_url( __FILE__ ) );
 define( 'SF_TEXTDOMAIN', 'source-framework' );
-
-if ( file_exists( plugin_dir_path( SF_FILENAME ) . 'source-framework.phar' ) ) {
-  $abspath = 'phar://' . plugin_dir_path( SF_FILENAME ) . 'source-framework.phar';
-} else {
-  $abspath = plugin_dir_path( SF_FILENAME ) . 'includes';
-}
-
-/**
- * Absolute path to the plugin or phar package
- * @since 1.0.0
- */
-define( 'SF_ABSPATH', $abspath );
+define( 'SF_ABSPATH', plugin_dir_path( SF_FILENAME ) . 'includes' );
 
 //Registering an autoload implementation
 spl_autoload_register( function($class_name) {
@@ -68,12 +55,10 @@ spl_autoload_register( function($class_name) {
     $class_filename = 'abstract-class-' . str_replace( '_', '-', end( $namespace ) );
     array_pop( $namespace );
     array_pop( $namespace );
-    $namespace[]    = 'core';
-  } elseif ( in_array( 'settings', $namespace ) ) {
-    $class_filename = 'class-' . str_replace( '_', '-', end( $namespace ) );
+  } else if ( in_array( 'traits', $namespace ) ) {
+    $class_filename = 'trait-' . str_replace( '_', '-', end( $namespace ) );
     array_pop( $namespace );
     array_pop( $namespace );
-    $namespace[]    = 'core';
   } else {
     $class_filename = 'class-' . str_replace( '_', '-', end( $namespace ) );
     array_pop( $namespace );
@@ -86,10 +71,22 @@ spl_autoload_register( function($class_name) {
 
   if ( file_exists( $filename ) ) {
     require $filename;
-  } else {
-    // developmet
-    var_dump( $class_name , $filename );
+  } else if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+
+    $backtrace = debug_backtrace();
+
+    foreach ( $backtrace as $info ) {
+      if ( empty( $info['file'] ) || $info['function'] != 'spl_autoload_call' ) {
+        continue;
+      }
+      break;
+    }
+
+    // development
+    var_dump( $class_name, $filename, $info['file'] );
     die;
+  } else {
+    wp_die( sprintf( __( "The class %s could not be loaded", SF_TEXTDOMAIN ), "<b><code>{$class_name}</code></b>" ) );
   }
 } );
 
@@ -100,20 +97,18 @@ add_action( 'plugins_loaded', [ 'SourceFramework\Core\Textdomain', 'load_plugin_
 add_action( 'all_plugins', [ 'SourceFramework\Core\Textdomain', 'modify_plugin_description' ] );
 
 // Check if the minimum requirements are met
-if ( version_compare( PHP_VERSION, '5.4.0', '<' ) ) {
+if ( version_compare( PHP_VERSION, '5.6.20', '<' ) ) {
 
   /**
    * Show notice for minimum PHP version required for SourceFramework
    * @since 2.0.0
    */
-  function source_framework_min_php_error() {
-    $message = __( 'SourceFramework requires PHP version 5.4 or later.', SF_TEXTDOMAIN );
+  add_action( 'admin_notices', function () {
+    $message = __( 'SourceFramework requires PHP version 5.6.20 or later.', SF_TEXTDOMAIN );
     printf( '<div class="notice notice-error is-dismissible"><p>%s</p></div>', esc_html( $message ) );
-  }
-
-  add_action( 'admin_notices', 'source_framework_min_php_error' );
+  } );
 } else {
 
   // Initialize SourceFramework
-  SourceFramework\Core\Init::init();
+  SourceFramework\Core\Init::get_instance();
 }
